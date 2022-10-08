d009a8d31d283893cf1df654a04f5ed5a5b672d1
yasser <yasser.4210149@eng.modern-academy.edu.eg>
2022-10-08 17:51:04 +0200

    all

--- app/routers/post.py
+++ app/routers/post.py
@@ -13,11 +13,11 @@ router = APIRouter(
 )
 #
 
-@router.get("/" )
-def get_posts(db: Session = Depends(get_db),
+@router.get("/",  response_model=List[schemas.PostOut] )
+def get_posts(db: Session = Depends(get_db,),
               current_user: str = Depends(oauth2.get_current_user), 
               
-             limit: int = 10, skip:int=0, search: Optional[str] = "" , response_model=List[schemas.PostOut]):
+             limit: int = 10, skip:int=0, search: Optional[str] = "" ):
  
   #ursor.execute("""SELECT * FROM posts""")
   #posts = cursor.fetchall()
@@ -27,7 +27,7 @@ def get_posts(db: Session = Depends(get_db),
    # posts= db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()    
     results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id==models.Post.id, 
      isouter = True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    return results
+    return [results]
 
 
 @router.post('/', status_code= status.HTTP_201_CREATED, response_model=list[schemas.Post])
@@ -46,7 +46,7 @@ def create_post(post:schemas.PostCreate, db: Session = Depends(get_db),
     db.commit()
     db.refresh(new_post)
     
-   # return new_post
+    return [new_post]
 #,response_model= schemas.PostOut
 @router.get("/{id}"  )
 def get_post(id: int, db: Session = Depends(get_db),
@@ -64,9 +64,8 @@ def get_post(id: int, db: Session = Depends(get_db),
     if not post: 
        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, 
                            detail= f"post with id {id} was not found")
-   #     response.status_code = status.HTTP_404_NOT_FOUND
-   #     return {'message':f"post with id {id} was not found"}
-    #return [post]   
+
+    return [post]  
    
 
 @router.delete("/{id}", status_code= status.HTTP_204_NO_CONTENT)
@@ -93,7 +92,7 @@ def delete_post(id:int,  db: Session = Depends(get_db),
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)  
 
-@router.put("/{id}", response_model= List[schemas.Post])
+@router.put("/{id}", response_model= schemas.Post)
 def update(id:int, post:schemas.PostCreate,  db: Session = Depends(get_db),
            current_user: int = Depends(oauth2.get_current_user)):
 #     cursor.execute(""" UPDATE  posts SET title= %s, content= %s, published=%s WHERE id= %s RETURNING *  """,
